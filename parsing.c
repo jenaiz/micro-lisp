50e93ce7e7a4f92ee7440cf7dd0aca8022c9d826
Jesús Navarrete <jesus.navarrete@gmail.com>
2014-04-12 21:05:15 +0200

    organizing functions and cleaning commented code

--- parsing.c
+++ parsing.c
@@ -43,6 +43,63 @@ enum { LVAL_NUM, LVAL_ERR};
 /* posible error types */
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM};
 
+
+lval lval_num(long x);
+lval lval_err(int x);
+void lval_print(lval v);
+lval eval_op(lval x, char* op, lval y);
+lval eval(mpc_ast_t* t);
+void lval_println(lval v);
+
+
+int main(int argc, char** argv) {
+  
+  /* Create Some Parsers */
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Microlisp    = mpc_new("microlisp");
+
+  /* Define them with the following Language */
+  mpca_lang(MPC_LANG_DEFAULT,
+    "                                                     \
+      number   : /-?[0-9]+/ ;                             \
+      operator : '+' | '-' | '*' | '/' ;                  \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      microlisp : /^/ <operator> <expr>+ /$/ ;            \
+    ",
+    Number, Operator, Expr, Microlisp);
+
+  puts("µLisp v0.0.1");
+  puts("[Apple LLVM version 5.1 (clang-503.0.38) (based on LLVM 3.4svn)]");
+  puts("Ctrl + c to exit.");
+  
+  while(1) {
+    char* input = readline("µL >> ");
+
+    add_history(input);
+    
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Microlisp, &r)) {      
+      lval result = eval(r.output);
+      lval_println(result);
+      
+      mpc_ast_delete(r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+
+    free(input);
+  }
+
+  /* Undefine and Delete our Parsers */
+  mpc_cleanup(4, Number, Operator, Expr, Microlisp);
+  
+  return 0;
+}
+
+/* functions... */
 lval lval_num(long x) {
   lval v;
   v.type = LVAL_NUM;
@@ -112,67 +169,6 @@ lval eval(mpc_ast_t* t) {
 }
 
 void lval_println(lval v) { lval_print(v); putchar('\n'); }
-
-int main(int argc, char** argv) {
-  
-  /* Create Some Parsers */
-  mpc_parser_t* Number   = mpc_new("number");
-  mpc_parser_t* Operator = mpc_new("operator");
-  mpc_parser_t* Expr     = mpc_new("expr");
-  mpc_parser_t* Microlisp    = mpc_new("microlisp");
-
-  /* Define them with the following Language */
-  mpca_lang(MPC_LANG_DEFAULT,
-    "                                                     \
-      number   : /-?[0-9]+/ ;                             \
-      operator : '+' | '-' | '*' | '/' ;                  \
-      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
-      microlisp : /^/ <operator> <expr>+ /$/ ;            \
-    ",
-    Number, Operator, Expr, Microlisp);
-
-  puts("µLisp v0.0.1");
-  puts("[Apple LLVM version 5.1 (clang-503.0.38) (based on LLVM 3.4svn)]");
-  puts("Ctrl + c to exit.");
-  
-  while(1) {
-    char* input = readline("µL >> ");
-
-    add_history(input);
-    
-    // printf("You are now %s\n", input);
-
-    mpc_result_t r;
-    if (mpc_parse("<stdin>", input, Microlisp, &r)) {
-      //mpc_ast_print(r.output); // print an Abstract Syntax Tree
-      
-      lval result = eval(r.output);
-      lval_println(result);
-      
-      /*mpc_ast_t* a = r.output;
-      printf("Tag: %s\n", a->tag);
-      printf("Contents: %s\n", a->contents);
-      printf("Number of children: %i\n", a->children_num);
-
-      mpc_ast_t* child_1 = a->children[0];
-      printf("First child Tag: %s\n", child_1->tag);
-      printf("First child Contents: %s\n", child_1->contents);
-      printf("First child Number of children: %i\n", child_1->children_num);
-      */
-      mpc_ast_delete(r.output);
-    } else {
-      mpc_err_print(r.error);
-      mpc_err_delete(r.error);
-    }
-
-    free(input);
-  }
-
-  /* Undefine and Delete our Parsers */
-  mpc_cleanup(4, Number, Operator, Expr, Microlisp);
-  
-  return 0;
-}
 
 
 
