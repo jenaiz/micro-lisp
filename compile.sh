f9fb000ee37859d1ae978c65bb948495e6e1bcc0
Jesús Navarrete <jesus.navarrete@gmail.com>
2014-04-15 22:05:49 +0200

    preparing for q-expressions

--- compile.sh
+++ compile.sh
@@ -3,4 +3,5 @@
 #cc -std=c99 -Wall doge_grammar.c mpc.c -ledit -lm -o doge_grammar
 #cc -std=c99 -Wall polish_notation.c mpc.c -ledit -lm -o polish_notation
 #cc -std=c99 -Wall parsing.c mpc.c -ledit -lm -o parsing
-cc -std=c99 -Wall s_expressions.c mpc.c -ledit -lm -o s_expressions
+#cc -std=c99 -Wall s_expressions.c mpc.c -ledit -lm -o s_expressions
+cc -std=c99 -Wall q_expressions.c mpc.c -ledit -lm -o q_expressions
