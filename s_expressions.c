32fe085c4688c753a86c2255da28f530fe5319b4
Jesús Navarrete <jesus.navarrete@gmail.com>
2014-04-15 23:14:55 +0200

    example of help from ruby

--- s_expressions.c
+++ s_expressions.c
@@ -5,29 +5,28 @@
 
 #ifdef _WIN32
 
-#include <string.h>
+  #include <string.h>
 
-static char buffer[2048];
+  static char buffer[2048];
 
-/* Fake readline function */
-char* readline(char* prompt) {
-  fputs(prompt, stdout);
-  fgets(buffer, 2048, stdin);
-  char* cpy = malloc(strlen(buffer)+1);
-  strcpy(cpy, buffer);
+  /* Fake readline function */
+  char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
 
-  cpy[strlen(ctr)-1] = '\0';
-  
-  return cpy;
-}
+    cpy[strlen(ctr)-1] = '\0';
+    
+    return cpy;
+  }
 
-/* Fake add_history function */
-
-void add_history(char* unused) {}
+  /* Fake add_history function */
+  void add_history(char* unused) {}
 
 #else
 
-#include <editline/readline.h>
+  #include <editline/readline.h>
 
 #endif
 
