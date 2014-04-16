22363c085ce7ae955c645aef16145f7ba215477c
Jesús Navarrete <jesus.navarrete@gmail.com>
2014-04-16 23:00:48 +0200

    cleaning, refactoring, organizing code

--- q_expressions.c
+++ q_expressions.c
@@ -2,32 +2,30 @@
 #include <stdlib.h>
 #include "mpc.h"
 
-
 #ifdef _WIN32
 
-#include <string.h>
+  #include <string.h>
 
-static char buffer[2048];
+  static char buffer[2048];
 
-/* Fake readline function */
-char* readline(char* prompt) {
-  fputs(prompt, stdout);
-  fgets(buffer, 2048, stdin);
-  char* cpy = malloc(strlen(buffer)+1);
-  strcpy(cpy, buffer);
+  /* Fake readline function */
+  char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
 
-  cpy[strlen(ctr)-1] = '\0';
-  
-  return cpy;
-}
+    cpy[strlen(ctr)-1] = '\0';
+    
+    return cpy;
+  }
 
-/* Fake add_history function */
-
-void add_history(char* unused) {}
+  /* Fake add_history function */
+  void add_history(char* unused) {}
 
 #else
 
-#include <editline/readline.h>
+  #include <editline/readline.h>
 
 #endif
 
@@ -35,15 +33,14 @@ void init() {
   puts("µLisp v0.0.4");
   puts("Ctrl + c to exit.");
 }
-/* Forward Declarations */
 
+/* Forward Declarations */
 struct lval;
 struct lenv;
 typedef struct lval lval;
 typedef struct lenv lenv;
 
 /* Lisp Value */
-
 enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_FUN, LVAL_SEXPR, LVAL_QEXPR };
 
 typedef lval*(*lbuiltin)(lenv*, lval*);
@@ -238,7 +235,6 @@ char* ltype_name(int t) {
 }
 
 /* Lisp Environment */
-
 struct lenv {
   int count;
   char** syms;
@@ -246,14 +242,12 @@ struct lenv {
 };
 
 lenv* lenv_new(void) {
-
   /* Initialize struct */
   lenv* e = malloc(sizeof(lenv));
   e->count = 0;
   e->syms = NULL;
   e->vals = NULL;
-  return e;
-  
+  return e;  
 }
 
 void lenv_del(lenv* e) {
@@ -455,12 +449,16 @@ void lenv_add_builtins(lenv* e) {
   
   /* List Functions */
   lenv_add_builtin(e, "list", builtin_list);
-  lenv_add_builtin(e, "head", builtin_head); lenv_add_builtin(e, "tail",  builtin_tail);
-  lenv_add_builtin(e, "eval", builtin_eval); lenv_add_builtin(e, "join",  builtin_join);
+  lenv_add_builtin(e, "head", builtin_head); 
+  lenv_add_builtin(e, "tail", builtin_tail);
+  lenv_add_builtin(e, "eval", builtin_eval); 
+  lenv_add_builtin(e, "join", builtin_join);
   
   /* Mathematical Functions */
-  lenv_add_builtin(e, "+",    builtin_add); lenv_add_builtin(e, "-",     builtin_sub);
-  lenv_add_builtin(e, "*",    builtin_mul); lenv_add_builtin(e, "/",     builtin_div);
+  lenv_add_builtin(e, "+", builtin_add); 
+  lenv_add_builtin(e, "-", builtin_sub);
+  lenv_add_builtin(e, "*", builtin_mul); 
+  lenv_add_builtin(e, "/", builtin_div);
 }
 
 /* Evaluation */
@@ -544,13 +542,13 @@ int main(int argc, char** argv) {
 
   /* Define them with the following Language */
   mpca_lang(MPC_LANG_DEFAULT,
-    "                                                                                         \
-      number : /-?[0-9]+/ ;                                                                   \
-      symbol : /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]+/ ;                                             \
-      sexpr  : '(' <expr>* ')' ;                                                              \
-      qexpr  : '{' <expr>* '}' ;                                                              \
-      expr   : <number> | <symbol> | <sexpr> | <qexpr> ;                                      \
-      microlisp  : /^/ <expr>* /$/ ;                                                          \
+    "                                                      \
+      number : /-?[0-9]+/ ;                                \
+      symbol : /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]+/ ;          \
+      sexpr  : '(' <expr>* ')' ;                           \
+      qexpr  : '{' <expr>* '}' ;                           \
+      expr   : <number> | <symbol> | <sexpr> | <qexpr> ;   \
+      microlisp  : /^/ <expr>* /$/ ;                       \
     ",
     Number, Symbol, Sexpr, Qexpr, Expr, Microlisp);
   init();
