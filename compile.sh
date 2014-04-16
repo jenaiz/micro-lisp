6fd3da644b1e460ceb69520b029ea456c2850304
Jesús Navarrete <jesus.navarrete@holidayinsider.com>
2014-04-16 13:13:57 +0200

    removing execution permissions & commented help compilation

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 rm -rf bin/
 mkdir bin
 cc -std=c99 -Wall read_file.c mpc.c -ledit -lm -o bin/read_file
-cc -std=c99 -Wall help.c mpc.c -ledit -lm -o bin/help
-#cc -std=c99 -Wall prompt.c -ledit -o bun/prompt
+#cc -std=c99 -Wall help.c mpc.c -ledit -lm -o bin/help
+#cc -std=c99 -Wall prompt.c -ledit -o bin/prompt
 #cc -std=c99 -Wall doge.c mpc.c -ledit -lm -o bin/doge
 #cc -std=c99 -Wall doge_grammar.c mpc.c -ledit -lm -o bin/doge_grammar
 #cc -std=c99 -Wall polish_notation.c mpc.c -ledit -lm -o bin/polish_notation
