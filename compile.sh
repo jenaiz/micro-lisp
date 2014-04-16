d7af71bbdd39e0b43e63181c77cb8bcb8f57ac12
Jesús Navarrete <jesus.navarrete@gmail.com>
2014-04-16 23:02:55 +0200

    q_expressions is now mlisp (preparing to use always the same source)

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 rm -rf bin/
 mkdir bin
-cc -std=c99 -Wall read_file.c mpc.c -ledit -lm -o bin/read_file
+#cc -std=c99 -Wall read_file.c mpc.c -ledit -lm -o bin/read_file
 #cc -std=c99 -Wall help.c mpc.c -ledit -lm -o bin/help
 #cc -std=c99 -Wall prompt.c -ledit -o bin/prompt
 #cc -std=c99 -Wall doge.c mpc.c -ledit -lm -o bin/doge
@@ -8,4 +8,4 @@ cc -std=c99 -Wall read_file.c mpc.c -ledit -lm -o bin/read_file
 #cc -std=c99 -Wall polish_notation.c mpc.c -ledit -lm -o bin/polish_notation
 #cc -std=c99 -Wall parsing.c mpc.c -ledit -lm -o bin/parsing
 #cc -std=c99 -Wall s_expressions.c mpc.c -ledit -lm -o bin/s_expressions
-cc -std=c99 -Wall q_expressions.c mpc.c -ledit -lm -o bin/q_expressions
+cc -std=c99 -Wall q_expressions.c mpc.c -ledit -lm -o bin/mlisp
